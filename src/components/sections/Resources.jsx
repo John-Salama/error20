55bb489e9b36733b9fd45f0e2c04235808804798
John-Salama <John.Salama.beshay@gmail.com>
2025-03-27 15:50:26 +0200

    feat: add download attribute to resource links for improved user experience

--- src/components/sections/Resources.jsx
+++ src/components/sections/Resources.jsx
@@ -38,6 +38,7 @@ const Resources = () => {
               </p>
               <a
                 href={resource.link}
+                download
                 className="flex items-center text-purple-600 font-medium hover:text-purple-800 transition-colors group"
               >
                 <Download className="w-5 h-5 mr-2" />
